adab5e0e2d1bed4b768cadc671df96857bb9aa52
Beverley Newing <bnewing92@hotmail.co.uk>
2020-08-07 12:50:21 +0100

    Run build to minify things

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '3cc30177d5c4385718af0780227adf5a');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'a766eafb81614c21ffb7015cc259c752');
