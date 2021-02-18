c25a287769569c79057ac397a3e0115405233423
RobLoweMOJ <RobLoweMOJ@users.noreply.github.com>
2021-02-18 13:28:47 +0000

    Run automated code lint

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,3 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '7f5b2bc7685441b3e9f73f5e863bc5f1');
+<?php
+
+return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '7f5b2bc7685441b3e9f73f5e863bc5f1');
--- mojblocks.php
+++ mojblocks.php
@@ -147,22 +147,22 @@ function mojblocks_register_blocks()
     );
 
 
-	register_block_type(
-		'mojblocks/banner',
-		[
-			'editor_script' => 'mojblocks-editor-script',
-			'render_callback' => 'render_callback_banner_block',
-			'attributes' => [
-				'bannerTitle' => [
-					'type' => 'string'
-				],
-				'bannerClassName' => [
-						'type' => 'string'
+    register_block_type(
+        'mojblocks/banner',
+        [
+            'editor_script' => 'mojblocks-editor-script',
+            'render_callback' => 'render_callback_banner_block',
+            'attributes' => [
+                'bannerTitle' => [
+                    'type' => 'string'
+                ],
+                'bannerClassName' => [
+                        'type' => 'string'
 
-				]
-			]
-		]
-	);
+                ]
+            ]
+        ]
+    );
 
     register_block_type(
         'mojblocks/accordion',
