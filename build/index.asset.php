3a3377fe35e38f844250b31dbca2c58d56a02d8a
RobLoweMOJ <RobLoweMOJ@users.noreply.github.com>
2021-02-19 11:46:38 +0000

    Run automated code lint

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,3 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'b9ade2512f3e2d6e11adefc0cec61fcb');
+<?php
+
+return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'b9ade2512f3e2d6e11adefc0cec61fcb');
