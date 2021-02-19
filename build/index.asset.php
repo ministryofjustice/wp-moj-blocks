0609f4ae817bec8770967f4ca83fd987e8888e0a
Robert Lowe <robert.lowe@digital.justice.gov.uk>
2021-02-19 11:57:42 +0000

    Asset Update

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,3 +1,1 @@
-<?php
-
-return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'b9ade2512f3e2d6e11adefc0cec61fcb');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'b9ade2512f3e2d6e11adefc0cec61fcb');
