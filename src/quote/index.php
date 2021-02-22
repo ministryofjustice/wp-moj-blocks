ece9bce434ee8606ad0d0cfb631b9862bb67a563
adam <brown.a2@gmail.com>
2021-02-22 15:31:30 +0000

    Write var conditionals to allow for editor selections
    Sometimes the editor may want the image or alignment added/changed and
    we don't want to have this markup in the code if it hasn't been
    selected.

--- src/quote/index.php
+++ src/quote/index.php
@@ -21,6 +21,19 @@ function render_callback_quote_block($attributes, $content)
     $attribute_quote_quoteName = $attributes['quoteName'] ?? '';
     $attribute_quote_quoteAlignment = $attributes['quoteAlignment'] ?? '';
 
+	// Add data-src only if image has been added
+	$data_src = !empty($attribute_quote_imgURL) ? 'data-src="'.
+	esc_url_raw($attribute_quote_imgURL) .'"' : '';
+
+    // Add alignment value and style only if alignment has been set by an editor
+    $quote_alignment = !empty($attribute_quote_quoteAlignment) ? 'style="text-align:' .
+	esc_html($attribute_quote_quoteAlignment) . '"' : '';
+
+    // Add image classes and styles only if there is an image set to display
+    $img_class = !empty($attribute_quote_imgURL) ?
+	'class="mojblocks-quote__image mojblocks-quote__image-selected" style="background-image:url(' .
+	esc_url_raw($attribute_quote_imgURL) . ');" ' : '';
+
     // Turn on buffering so we can collect all the html markup below and load it via the return
     // This is an alternative method to using sprintf(). By using buffering you can write your
     // code below as you would in any other PHP file rather then having to use the sprintf() syntax
@@ -28,14 +41,11 @@ function render_callback_quote_block($attributes, $content)
 
     ?>
 
-    <div class="<?php _e(esc_html($attribute_quote_className)) ; ?> mojblocks-quote"
-        data-src="<?php _e(esc_url_raw($attribute_quote_imgURL)); ?>">
-        <div class="mojblocks-quote__image mojblocks-quote__image-selected"
-            style="background-image:url('<?php _e(esc_url_raw($attribute_quote_imgURL)); ?>');">
+    <div class="<?php _e(esc_html($attribute_quote_className)) ; ?> mojblocks-quote" <?php _e($data_src); ?> >
+        <div <?php _e($img_class); ?> >
         </div>
         <div class="govuk-width-container">
-            <div class="mojblocks-quote__content"
-                style="text-align: <?php _e(esc_html($attribute_quote_quoteAlignment)) ; ?>">
+            <div class="mojblocks-quote__content" <?php _e($quote_alignment); ?> >
                 <div class="mojblocks-quote__content__icon">
                     <span class="dashicon dashicons dashicons-format-quote"></span>
                 </div>
