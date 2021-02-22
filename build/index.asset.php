c5315e489560565a6d8765f30254303fad81d131
adam <brown.a2@gmail.com>
2021-02-22 13:35:14 +0000

    Final compiling of assets

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,3 +1,1 @@
-<?php
-
-return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '351abce435e3159fce1f8ff47e7cd7e9');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'd1e7b683932c63db292866a88df47a6b');
