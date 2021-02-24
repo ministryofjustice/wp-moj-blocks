317cfce5217406cbbb00c00c24c7248b4d7886fb
adam <brown.a2@gmail.com>
2021-02-24 16:48:04 +0000

    Fix editor remove icon color
    The remove icon was a dark blue on black that you could hardly see, this
    makes it white so you can see it more clearly.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,3 +1,1 @@
-<?php
-
-return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '329c74aa287e979496be01e769319cab');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '4c1d6e416d87b820fcca4d23b43cef09');
