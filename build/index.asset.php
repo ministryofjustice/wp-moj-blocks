10332d7176640ae4aeab43a60585af8c520a2342
adam <brown.a2@gmail.com>
2021-03-02 11:36:59 +0000

    Recompile CSS

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,3 +1,1 @@
-<?php
-
-return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '22a9348902f752f5e2f1541bdb76b7f5');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '17ade6aa719fc944aadf3d58be30a572');
