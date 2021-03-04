420358032d1fc0db83a952197ec02ebdfd5a73fe
adam <brown.a2@gmail.com>
2021-03-04 16:27:54 +0000

    Bug fix, wrong variable name
    The wrong variable name was used causing an important block class not to
    load and mixing up the div loading on the page.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,3 +1,1 @@
-<?php
-
-return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '17ade6aa719fc944aadf3d58be30a572');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '17ade6aa719fc944aadf3d58be30a572');
--- src/intro/index.php
+++ src/intro/index.php
@@ -25,7 +25,7 @@ function render_callback_intro_block($attributes, $content)
 
     ?>
 
-    <div class="<?php _e(esc_html($attribute_quote_className)); ?>">
+    <div class="<?php _e(esc_html($attribute_intro_className)); ?>">
         <div class="govuk-width-container">
             <div class="govuk-grid-row">
                 <div class="govuk-grid-column-three-quarters">
