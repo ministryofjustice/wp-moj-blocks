5a8f5d1ea47fad412088d8b8ccedc2b17176045d
Robert Lowe <robert.lowe@digital.justice.gov.uk>
2021-03-16 12:03:48 +0000

    Dynamic Block - Highlights List

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,3 +1,1 @@
-<?php
-
-return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'f5512d9ee8f2d55609847576dfdf858b');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'e737d9267216d06e2fad671e31372abf');
