07bbf7a51922fa1402133b786535405c821acec3
Robert Lowe <robert.lowe@digital.justice.gov.uk>
2021-03-23 15:20:22 +0000

    Fix typos

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,3 +1,1 @@
-<?php
-
-return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '0240de3f1e4308a56bbe9bcdeb937de6');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '64683431f5d80f4e72b1a9064090e0b2');
