6f57c40f9c582ff71c481464b3970f2587ae1c39
adam <brown.a2@gmail.com>
2021-04-13 12:04:09 +0100

    Remove loading accordion block Gov UK JS
    This has been removed as the Hale theme that these blocks have been
    designed for, loads Gov UK JS already.

--- src/mojblocks.js
+++ src/mojblocks.js
@@ -4,16 +4,12 @@
  */
 
 // import dependencies
-import { Accordion } from 'govuk-frontend';
 import { Details } from 'govuk-frontend';
 
 // get components
-/** Accordion **/
-const mojblocksAccordion = document.querySelector('[data-module="govuk-accordion"]') || null;
 
 /** Reveal **/
 const mojblocksReveal = document.querySelector('[data-module="govuk-details"]') || null;
 
 // initialise components
-if (mojblocksAccordion) { new Accordion(mojblocksAccordion).init() }
 if (mojblocksReveal) { new Details(mojblocksReveal).init() }
