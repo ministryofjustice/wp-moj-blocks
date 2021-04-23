9c28fe37ea8854d6d1ffa1255bd670041387bad5
adam <brown.a2@gmail.com>
2021-04-23 17:55:41 +0100

    Hightlights Block remove NHS style

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '501ac59a54fd56dd9af0305f0d2251ab');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '8f0459ef9a3c58b4df22ef1f4ddece30');
