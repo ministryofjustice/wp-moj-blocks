da7a498cf4da0b1e80524b596599c6a101c06137
brown-a2 <brown-a2@users.noreply.github.com>
2021-04-27 18:34:04 +0000

    Run automated code lint

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,3 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '858ce48c2fd8efeb35fae1f5e9576b00');
+<?php
+
+return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '858ce48c2fd8efeb35fae1f5e9576b00');
