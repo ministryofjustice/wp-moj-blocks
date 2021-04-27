dd2918c820bdc52be7537b3ae9909b591199c596
adam <brown.a2@gmail.com>
2021-04-27 19:33:28 +0100

    Update and compile assets

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,3 +1,1 @@
-<?php
-
-return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'e26eae2ba240715ca4cb7e99bb45be25');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '858ce48c2fd8efeb35fae1f5e9576b00');
