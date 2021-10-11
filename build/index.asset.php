dd6d1fa54a38234de8a6a427946406c2dd536bdc
adam <brown.a2@gmail.com>
2021-10-11 17:11:13 +0100

    Recompile code with newly merged main branch

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '0a665505a626bfdb0201b2539c18747f');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '23e74c9eb284662c9c58ea54feb2be65');
