044b91eee2ea348de2f89de56b3a099514cb46b0
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2021-10-18 12:21:34 +0100

    tidy up

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'd765ac7f7f3966db03b118edc55f44c4');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '17cc60c058149a43c410419515364ee8');
--- src/custom-blocks/featured-news/index.js
+++ src/custom-blocks/featured-news/index.js
@@ -1,16 +1,5 @@
 const { registerBlockType, registerBlockStyle } = wp.blocks;
 const { __ } = wp.i18n;
-/*
-const { Fragment } = wp.element;
-// const { RichText, MediaUpload, InspectorControls, URLInputButton } = wp.blockEditor;
-const ALLOWED_MEDIA_TYPES = ['image'];
-const { PanelBody } = wp.components;
-const templateFeaturedNewsBlock = [
-  [ 'core/heading', { placeholder: 'Add featured news section title' } ]
-];
-const d = new Date();
-*/
-
 
 import { InnerBlocks } from "@wordpress/block-editor";
 
@@ -38,88 +27,6 @@ registerBlockType("mojblocks/featured-news", {
       type: "string"
     }
   },
-/*
-  edit: props => {
-
-    const {
-      attributes: { featuredNewsLink },
-        className,
-        setAttributes
-    } = props
-
-    const onChangefeaturedNewsLink = newFeaturedNewsLink => {
-      setAttributes({ featuredNewsLink: newFeaturedNewsLink})
-    }
-
-    const [ hasDate, setHasDate ] = useState( true );
-    const [ story, setStory ] = useState( 'none' );
-
-    return (
-      <Fragment >
-        <InspectorControls>
-          <PanelBody
-            title={__('News settings')}
-            initialOpen={true}
-          >
-            <SelectControl
-              label="Select news"
-              value={ story }
-              options={ optionList }
-              onChange={ ( newStory ) => setStory( newStory ) }
-            />
-            <ToggleControl
-              label="Show/hide article dates"
-              help={
-                hasDate
-                ? 'The date will be displayed'
-                : 'The date will be hidden'
-              }
-              checked={ hasDate }
-              onChange={ setAttributes({ featuredNewsHasDate: hasDate } ) }
-              onChange={ () => {
-                setHasDate( ( state ) => ! state );
-              } }
-            />
-          </PanelBody>
-        </InspectorControls>
-
-        <div className={`mojblocks-featured-news mojblocks-featured-news--${story} ${className}`}>
-          <div className="govuk-width-container">
-            <InnerBlocks
-              template={ templateFeaturedNewsBlock }
-              templateLock="all"
-            />
-            <div className={`govuk-grid-row ${hasDate ? '' : 'mojblocks-featured-news-hide-date'} ` }>
-              <div class="mojblocks-featured-news__item">
-                <div class="mojblocks-featured-news__image">
-                </div>
-                <div className="mojblocks-featured-news__text">
-                  <div className="govuk-body govuk-!-font-size-24 govuk-!-font-weight-bold mojblocks-featured-news__headline" >
-                    { title }
-                  </div>
-                  <div className="govuk-body mojblocks-featured-news__summary" >
-                    { summary }
-                  </div>
-                  <div className="govuk-body-s mojblocks-featured-news__date" >
-                    { datify(date,d) }
-                  </div>
-                  <RichText
-                    tagName="div"
-                    value={featuredNewsLink ? 'Read full article' : featuredNewsLink }
-                    onChange={onChangefeaturedNewsLink}
-                    className="govuk-button mojblocks-featured-news__link"
-                    placeholder={__('Read full article', 'mojblocks')}
-                    keepPlaceholderOnFocus={true}
-                  />  
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </Fragment>
-    );
-  },
-*/
-    edit, 
-    save: () => { return <InnerBlocks.Content />; }
+  edit, 
+  save: () => { return <InnerBlocks.Content />; }
 });
