21dfe776867271e47127ed6b3b1453f97ff9c46b
Robert Lowe <robert.lowe@digital.justice.gov.uk>
2022-01-17 09:56:15 +0000

    Add flexible heading level using inner block on banner block

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '15f01eab8d6ea90ea4dcc6281919ecaa');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '614f3e36db5dff7a5e79f6d1a6f8511e');
