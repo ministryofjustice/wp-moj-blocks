eaca69ce9398501e08561d8b889dda8876c05fa4
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2022-07-13 10:59:26 +0100

    Removed styles, superficial changes to HTML, NPM install with GDS DS 4.2.0

--- src/custom-blocks/accordion/index.php
+++ src/custom-blocks/accordion/index.php
@@ -63,17 +63,17 @@ function render_callback_accordion_block_section($attributes, $content)
 
     <div class="<?php _e(esc_html($attribute_accordion_section_className)) ; ?> govuk-accordion__section">
         <div class="govuk-accordion__section-header">
-        <h3 class="govuk-accordion__section-heading">
-            <span class="govuk-accordion__section-button" id="accordion-default-heading-1">
-            <?php _e(esc_html($attribute_accordion_section_Title)) ; ?>
-            </span>
-        </h3>
+            <h3 class="govuk-accordion__section-heading">
+                <span class="govuk-accordion__section-button" id="accordion-default-heading-1">
+                <?php _e(esc_html($attribute_accordion_section_Title)) ; ?>
+                </span>
+            </h3>
         </div>
         <div id="accordion-default-content-1" class="govuk-accordion__section-content" aria-labelledby="accordion-default-heading-1">
-            <div class="govuk-body">
-            <?php _e(esc_html($attribute_accordion_section_TextArea)) ; ?>
-            <?php _e(esc_html($content)); ?>
-            </div>
+            <p class="govuk-body">
+                <?php _e(esc_html($attribute_accordion_section_TextArea)) ; ?>
+                <?php _e(esc_html($content)); ?>
+            </p>
         </div>
     </div>
 
