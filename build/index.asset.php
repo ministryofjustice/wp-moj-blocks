4c406fcb393238bef198f5de6c48b858a359561d
Robert Lowe <robert.lowe@digital.justice.gov.uk>
2022-11-11 09:29:59 +0000

    Version Bump 3.8.0

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'e5398669161ec2b086fc0d2f14a70c59');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '51eeebc19f28a87d1546e75fcbc930de');
