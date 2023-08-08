ead045bbed15f6bf9b3d00aa7dd504abc2b57555
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2023-08-08 10:12:19 +0100

    whitespace

--- src/custom-blocks/card/edit.js
+++ src/custom-blocks/card/edit.js
@@ -39,7 +39,7 @@ export default function CardBlockEdit( props ) {
         };
 
         const shapeList = [
-            { label: "1:1 (square)", value: '100'},
+            { label: "1:1 (square)", value: '100' },
             { label: "4:3 (rectangle)", value: '75' },
             { label: "16:9 (widescreen)", value: '56' },
             { label: "21:9 (letterbox)", value: '43' },
