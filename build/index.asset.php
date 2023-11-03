79543c11da9d1107974f56c82bcd9063ab6a5953
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2023-11-03 16:46:57 +0000

    changes for the GDS design system v5

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'c0c5e60328a91312baad');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'ea86809a25a1bf327f15');
