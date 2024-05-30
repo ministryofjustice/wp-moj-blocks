01b1739dfb3394fb6b28ec61e28c362bddc54c5d
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2024-05-30 12:48:59 +0100

    build

--- src/extended-core-blocks/group/index.js
+++ src/extended-core-blocks/group/index.js
@@ -5,7 +5,10 @@
  */
 
 import { __ } from '@wordpress/i18n';
-import { registerBlockStyle, unregisterBlockStyle } from '@wordpress/blocks';
+import { registerBlockStyle, registerBlockExtension } from '@wordpress/blocks';
+const { InspectorControls, PanelColorSettings } = wp.blockEditor;
+const { PanelBody, PanelRow } = wp.components;
+import { ToggleControl, RangeControl } from '@wordpress/components';
 
 registerBlockStyle( 'core/group', {
     name: 'bleeding-background',
@@ -21,3 +24,63 @@ registerBlockStyle( 'core/group', {
     name: 'faded-bleed',
     label: __('Fading out', 'mojblocks'),
 } );
+
+registerBlockExtension(
+    'core/group',
+    {
+        extensionName: 'outline',
+        attributes: newAttributes,
+        inlineStyleGenerator: generateInlineStyle,
+        Edit: outlineBlockEdit
+    }
+);
+
+const newAttributes = {
+    hasOutline: {
+        type: "boolean",
+        default: false
+    },
+    outlineThickness: {
+        type: "number",
+        default: 3
+    },
+    outlineColour: {
+        type: 'string',
+        default: "rgb(255,255,255)"
+    },
+};
+
+function outlineBlockEdit( props ) {
+    const { attributes, setAttributes } = props;
+    const { hasOutline, outlineThickness, outlineColour } = attributes;
+
+    return (
+        <InspectorControls>
+            <PanelBody title={__("Outline options", "mojblocks" )}>
+                <ToggleControl
+                    label="Outline"
+                    checked={ hasOutline }
+                    onChange={ value => setAttributes({ hasOutline: value }) }
+                />
+                <RangeControl
+                    label={__("Thickness", "mojblocks" )}
+                    help=""
+                    value={ outlineThickness }
+                    onChange={ value => setAttributes({ outlineThickness: value }) }
+                    min={ 1 }
+                    max={ 6 }
+                />
+                <PanelColorSettings
+                    title={__("Colour", "mojblocks" )}
+                    colorSettings={[
+                        {
+                            value: outlineColour,
+                            onChange: value => setAttributes({ outlineColour: value }),
+                            label: __('Outline colour', 'mojblocks')
+                        }
+                    ]}
+                />
+            </PanelBody>
+        </InspectorControls>
+    );
+}
