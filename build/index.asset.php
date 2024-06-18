72527991e2a293d9bfe8a90c16264238f6785cc6
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2024-06-18 17:31:37 +0100

    Stuff

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '6e560d6f2a609a62d43e');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '1399fe4e262312c5f489');
