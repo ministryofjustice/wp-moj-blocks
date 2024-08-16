96a376d0adc22c1fc932647b8fd33d0ea2117af6
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2024-08-16 14:10:58 +0100

    Added code to change item type

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '3709571cb024fd54a722');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'd45a6104c02b456baf9a');
