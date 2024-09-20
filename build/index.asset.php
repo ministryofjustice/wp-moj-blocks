d301388815ba715891b10776b409f0d5e600d41f
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2024-09-20 13:50:11 +0100

    Added new Latest Item block

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '56e46154a1d993676bfd');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '3cff490684c7a49e1886');
