4af654c866a1cb3729f92faf982fa6b8da162790
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2024-09-26 12:02:26 +0100

    Removed API script to be included in Hale theme

--- mojblocks.php
+++ mojblocks.php
@@ -556,33 +556,3 @@ function mojblocks_enqueue_style()
 }
 
 add_action('wp_enqueue_scripts', 'mojblocks_enqueue_style');
-
-function mojblocks_extend_wp_api($data, $post, $context) {
-
-    // Add featured image to API
-    $featured_image_id = $data->data['featured_media']; // get featured image id
-    $featured_image_url = wp_get_attachment_image_src( $featured_image_id, 'feature' ); // get url of the feature size
-
-    if( $featured_image_url ) {
-        $data->data['featured_image_url'] = $featured_image_url[0];
-    } else {
-        $data->data['featured_image_url'] = "";
-    }
-
-    // Add summary to API
-    $summary = get_post_meta( get_the_ID(), 'post_summary', TRUE); // get the value from the meta field
-    $data->data['post_meta'] = array( 'summary' => $summary );
-
-    return $data;
-}
-
-function add_all_prepare_filters() {
-    $args = array(
-        'public'   => true,
-    );
-    $post_types = get_post_types($args);
-    foreach ($post_types as $post_type) {
-        add_filter( "rest_prepare_$post_type", 'mojblocks_extend_wp_api', 10, 3 );
-    }
-}
-add_action( 'pre_get_posts', 'add_all_prepare_filters' );
