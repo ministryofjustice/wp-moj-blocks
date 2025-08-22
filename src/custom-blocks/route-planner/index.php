287325a94229fe9dc0806165c710163e8989490e
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2025-08-22 11:58:31 +0100

    reduce JS repetition

--- src/custom-blocks/route-planner/index.php
+++ src/custom-blocks/route-planner/index.php
@@ -37,26 +37,29 @@ function render_callback_route_planner_block($attributes, $content)
                 <span class="govuk-visually-hidden">Error:</span> Enter a postcode or address.
             </p>
             <input class="govuk-input govuk-input--width-10 govuk-!-margin-bottom-4" type="text" id="<?php echo $rand_num;?>-postcodeInput" aria-describedby="<?php echo $rand_num;?>-postcodeInput-hint"/><br />
-            <button class="govuk-button" onclick="redirectToMap<?php echo $rand_num;?>(event)"><?php _e("Submit","hale"); ?></button>
+            <button class="govuk-button" onclick="redirectToMap(event,'<?php echo esc_html($attribute_route_destination); ?>',<?php echo $rand_num;?>)"><?php _e("Submit","hale"); ?></button>
         </form>
         <script>
-            function redirectToMap<?php echo $rand_num;?>(e) {
-                e.preventDefault();
-                const postcode = document.getElementById("<?php echo $rand_num;?>-postcodeInput").value.trim();
-                if (!postcode) {
-                    document.getElementById("<?php echo $rand_num;?>-postcodeInput-error").style.display = "block";
-                    document.getElementById("<?php echo $rand_num;?>-postcodeInput").classList.add("govuk-input--error");
-                    return;
+            if (typeof window.redirectToMap !== 'function') {
+                function redirectToMap(e,dest,idPrefix) {
+                    e.preventDefault();
+                    const postcode = document.getElementById(idPrefix+"-postcodeInput").value.trim();
+                    console.log(postcode);
+                    if (!postcode) {
+                        document.getElementById(idPrefix+"-postcodeInput-error").style.display = "block";
+                        document.getElementById(idPrefix+"-postcodeInput").classList.add("govuk-input--error");
+                        return;
+                    }
+                    document.getElementById(idPrefix+"-postcodeInput-error").style.display = "none";
+                    document.getElementById(idPrefix+"-postcodeInput").classList.remove("govuk-input--error");
+
+                    const origin = encodeURIComponent(postcode);
+                    const destination = encodeURIComponent(dest); // your fixed location
+
+                    const url = `https://www.google.com/maps/dir/?api=1&origin=${origin}&destination=${destination}&travelmode=<?php echo esc_html($attribute_route_method); ?>`;
+
+                    window.open(url, '_blank').focus();
                 }
-                document.getElementById("<?php echo $rand_num;?>-postcodeInput-error").style.display = "none";
-                document.getElementById("<?php echo $rand_num;?>-postcodeInput").classList.remove("govuk-input--error");
-
-                const origin = encodeURIComponent(postcode);
-                const destination = encodeURIComponent("<?php echo esc_html($attribute_route_destination); ?>"); // your fixed location
-
-                const url = `https://www.google.com/maps/dir/?api=1&origin=${origin}&destination=${destination}&travelmode=<?php echo esc_html($attribute_route_method); ?>`;
-
-                window.open(url, '_blank').focus();
             }
         </script>
 
